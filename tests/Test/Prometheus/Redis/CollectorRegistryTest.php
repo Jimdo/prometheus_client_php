743122bc349123c5e74aab71b96371b2fff4f1ed
Julius Ehrlich <julius@ehrlich-bros.de>
2018-08-09 11:20:16 +0200

    Added a unit test for the Redis flushing to ensure only metric data is being flushed

--- tests/Test/Prometheus/Redis/CollectorRegistryTest.php
+++ tests/Test/Prometheus/Redis/CollectorRegistryTest.php
@@ -4,6 +4,8 @@
 namespace Test\Prometheus\Redis;
 
 use function class_exists;
+use Prometheus\CollectorRegistry;
+use Prometheus\Counter;
 use Prometheus\Storage\Redis;
 use Test\Prometheus\AbstractCollectorRegistryTest;
 
@@ -16,5 +18,29 @@ class CollectorRegistryTest extends AbstractCollectorRegistryTest
     {
         $this->adapter = new Redis(array('host' => REDIS_HOST));
         $this->adapter->flushRedis();
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldOnlyFlushMetricData()
+    {
+        $redis = new \Redis();
+        $redis->connect(REDIS_HOST);
+        $redis->set('foo', 'bar');
+
+        $registry = new CollectorRegistry($this->adapter);
+        $counter = $registry->registerCounter('test', 'some_counter', 'it increases', ['type']);
+        $counter->incBy(6, ['blue']);
+
+        $counterRedisKey = 'PROMETHEUS_' . Counter::TYPE . Redis::PROMETHEUS_METRIC_KEYS_SUFFIX;
+        $this->assertEquals(['PROMETHEUS_:counter:test_some_counter'], $redis->sMembers($counterRedisKey));
+
+        $this->adapter->flushRedis();
+
+        $this->assertEquals('bar', $redis->get('foo'));
+        $this->assertEquals([], $redis->sMembers($counterRedisKey));
+
+        $redis->del('foo');
     }
 }
